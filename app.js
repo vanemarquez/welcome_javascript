0eae93092a214d3748901b1720464cb2c83fbfab
Vanessa Marquez <vaneyz0302@gmail.com>
2024-05-09 23:39:49 -0500

    Se modificó app.js index.html readme.md styles.css

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("Hola mundo");
+
+/*
+JavaScript
+
+1. Concepto
+- Es un lenguaje de programación interpretado ampliamente utilizado para crear contenido interactivo en páginas web.
+
+2. Características
+- Permite ejecutar código directamente en el navegador sin necesidad de compilación previa.
+- Tiene un lenguaje dinámico, esto permite que sea muy flexible.
+- Es uno de los lenguajes de programación más utilizados en el desarrollo web.
+- Cuenta con un amplio ecosistema de bibliotecas y frameworks.
+
+3. ¿ Qué es una variable?
+- Es un espacio en la memoria RAM que nos permite guardar un valor, es decir, nos permite guardar información.
+
+4. Variables en JavaScript
+- VAR (ES5): Usa var para variables con alcance de función.
+- LET (ES6): Usa let para variables con alcance de bloque que pueden cambiar su valor.
+- CONST (ES6): Usa const para variables con alcance de bloque que no cambiarán su valor después de la asignación inicial.
+*/
+
+/*
+5. Tipos de Datos
+- String: Representa texto y se encierra entre comillas.
+Ejemplo:*/ 
+let city="Lima";
+console.log(city);
+
+/*- Numérico: Representa valores numéricos. Pueden ser números enteros o decimales.
+Ejemplo:*/
+let sum= 10+10;
+console.log(sum); 
+/*
+- Boolean: Representa un valor lógico. Permiten solamente dos tipos de valores, true or false.
+Este tipo se utiliza comúnmente para almacenar valores de sí/no: true significa “sí, correcto, verdadero”, y 
+false significa “no, incorrecto, falso”.
+Ejemplo:*/
+let haveCard= true;
+console.log("El usuario tiene una tarjeta de crédito");
+
+
+
---
+++ readme.md
@@ -0,0 +1,23 @@
+# JavaScript
+
+## 1. Concepto
+- Es un lenguaje de programación interpretado ampliamente utilizado para crear contenido interactivo en páginas web.
+
+## 2. Características
+- Permite ejecutar código directamente en el navegador sin necesidad de compilación previa.
+- Tiene un lenguaje dinámico, esto permite que sea muy flexible.
+- Es uno de los lenguajes de programación más utilizados en el desarrollo web.
+- Cuenta con un amplio ecosistema de bibliotecas y frameworks.
+
+## 3. ¿ Qué es una variable?
+- Es un espacio en la memoria RAM que nos permite guardar un valor, es decir, nos permite guardar información.
+
+## 4. Variables en JavaScript
+- VAR (ES5): Usa var para variables con alcance de función.
+- LET (ES6): Usa let para variables con alcance de bloque que pueden cambiar su valor.
+- CONST (ES6): Usa const para variables con alcance de bloque que no cambiarán su valor después de la asignación inicial.
+
+## 5. Tipos de Datos
+- String: Representa texto y se encierra entre comillas.
+- Numérico: Representa valores numéricos, enteros y decimales.
+- Boolean: Representa un valor lógico, true or false.
